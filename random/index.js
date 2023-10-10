662d1476969f8b9efbbe223c7e54032cd0e5cce6
Andrey Khalepov <w54661c@gmail.com>
2023-10-10 22:15:53 +1000

    random in range

--- random/index.js
+++ random/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function randomInRange(min, max) {
-    return Math.trunc(min + Math.random() * (max - min));
+    return Math.trunc(min + Math.random() * (max - min + 1));
 }
 
-console.log(randomInRange(10, 30));
+console.log(randomInRange(1, 6));
