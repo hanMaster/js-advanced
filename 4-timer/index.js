27dddf6a201b238bce60f745df3ccc8524481c19
Andrey Khalepov <w54661c@gmail.com>
2023-10-12 19:46:41 +1000

    timer done

--- 4-timer/index.js
+++ 4-timer/index.js
@@ -2,41 +2,61 @@
 
 const slot = document.getElementById('slot');
 
-const displayTime = (time) => {
-    return new Intl.DateTimeFormat('ru-RU').format(time);
+const formatter = new Intl.RelativeTimeFormat('ru-RU');
+
+const year = new Date().getFullYear();
+const newYear = new Date(year + 1, 0, 1, 0, 0, 0, 0);
+
+const countDaysInCurrentMonth = () => {
+    const today = new Date();
+    const m31 = [1, 3, 5, 7, 8, 10, 12];
+    const month = today.getMonth() + 1;
+
+    if (m31.includes(month)) {
+        return 31;
+    }
+
+    if (today.getFullYear() % 4 === 0 && month === 2) {
+        return 29;
+    }
+
+    if (today.getFullYear() % 4 !== 0 && month === 2) {
+        return 28;
+    }
+
+    return 30;
 };
 
-const formatter = new Intl.RelativeTimeFormat('ru-RU', { numeric: 'auto' });
-
-const displayInterval = (time) => {
-    const now = new Date();
-    console.log('hours', now.getHours());
-    const M = formatter.formatToParts(11 - now.getMonth(), 'month');
-    const d = formatter.formatToParts(0, 'day');
-    const h = formatter.formatToParts(24 - now.getHours(), 'hour');
-    const m = formatter.formatToParts(27, 'minute');
-    const s = formatter.formatToParts(2, 'second');
-
-    return `${M[1].value}${M[2].value}, ${d[1].value}${d[2].value}, ${h[1].value}${h[2].value}, ${m[1].value}${m[2].value}, ${s[1].value}${s[2].value}`;
+const calcDays = () => {
+    return countDaysInCurrentMonth() - new Date().getDate();
 };
 
-const year = new Date().getFullYear() + 1;
+const setEstimate = () => {
+    const diff = newYear.getTime() - Date.now();
 
-const newYear = new Date(year, 0, 1, 0, 0, 0);
+    const diffMonth = 11 - new Date().getMonth();
+    const month = formatter.formatToParts(diffMonth, 'months');
+    slot.innerText = `${month[1].value}${month[2].value},`;
 
-console.log('New Year', newYear);
+    const diffDays = calcDays();
+    const days = formatter.formatToParts(diffDays, 'days');
+    slot.innerText += ` ${days[1].value}${days[2].value}, `;
 
-const diff = newYear.getTime() - Date.now();
+    const diffHours = Math.floor((diff / 1000 / 60 / 60) % 24);
+    const hrs = formatter.formatToParts(diffHours, 'hours');
+    slot.innerText += ` ${hrs[1].value}${hrs[2].value}, `;
 
-console.log(displayTime(diff));
+    const diffMinutes = Math.floor((diff / 1000 / 60) % 60);
+    const min = formatter.formatToParts(diffMinutes, 'minutes');
+    slot.innerText += ` ${min[1].value}${min[2].value}, `;
 
-slot.innerText = displayInterval(new Date(diff));
+    const diffSeconds = Math.floor((diff / 1000) % 60);
+    const sec = formatter.formatToParts(diffSeconds, 'seconds');
+    slot.innerText += ` ${sec[1].value}${sec[2].value}`;
+};
 
-// const countDown = () => {
-//     setInterval(() => {
-//         now -= 1000;
-//         console.log(displayTime(now));
-//     }, 1000);
-// };
+setEstimate();
 
-// countDown();
+setInterval(() => {
+    setEstimate();
+}, 1000);
