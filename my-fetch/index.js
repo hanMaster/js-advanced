3983d3c644cfd685e03b479bf1ce0729ad33f003
hanmaster <w54661c@gmail.com>
2023-10-19 13:13:42 +1000

    myFetch done

--- my-fetch/index.js
+++ my-fetch/index.js
@@ -4,11 +4,11 @@ const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/ditto2';
 
 function myFetch(url) {
     return new Promise((reslolve, reject) => {
-        const req1 = new XMLHttpRequest();
-        req1.open('GET', url);
-        req1.send();
+        const req = new XMLHttpRequest();
+        req.open('GET', url);
+        req.send();
 
-        req1.addEventListener('load', function () {
+        req.addEventListener('load', function () {
             if (this.status === 200) {
                 reslolve(JSON.parse(this.responseText));
             }
