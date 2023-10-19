6649f1df30dde9829146976f03b12ecde476c30d
hanmaster <w54661c@gmail.com>
2023-10-19 14:19:45 +1000

    geo city done

---
+++ get-city/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const slot = document.querySelector('span');
+const url = 'https://api.bigdatacloud.net/data/reverse-geocode-client?';
+
+async function getGeolocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                resolve({ latitude: pos.coords.latitude, longitude: pos.coords.longitude });
+            },
+            (err) => reject(new Error(err.message))
+        );
+    });
+}
+
+const run = async () => {
+    try {
+        const coords = await getGeolocation();
+        const fullUrl = `${url}latitude=${coords.latitude}&longtitude=${coords.longitude}`;
+        const response = await fetch(fullUrl);
+        const data = await response.json();
+        slot.innerText = data.city;
+    } catch (err) {
+        slot.innerText = `Ошибка получения геопозиции: ${err.message}`;
+        slot.style.color = 'salmon';
+    }
+};
+
+run();
