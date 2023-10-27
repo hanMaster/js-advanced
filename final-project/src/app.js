cd7fb96b982573b0193900c0a3174b758a80f275
hanmaster <w54661c@gmail.com>
2023-10-27 19:03:11 +1000

    Counter done

--- final-project/src/app.js
+++ final-project/src/app.js
@@ -1,5 +1,4 @@
 import { routes } from './routes';
-import './app.css';
 import { NotFoundView } from './views/404/NotFoundView.js';
 
 class App {
--- final-project/src/components/header/header.js
+++ final-project/src/components/header/header.js
@@ -1,18 +1,37 @@
-import onChange from 'on-change';
+// import onChange from 'on-change';
 import { DivComponent } from '../../common/DivComponent';
 import './header.css';
 
 export class Header extends DivComponent {
     constructor(appState) {
         super();
-        this.appState = onChange(appState);
+        this.appState = appState;
     }
 
     build() {
         this.el.innerHTML = '';
         this.el.classList.add('header');
         this.el.innerHTML = `
-        
+        <div>
+            <img src="public/images/logo.svg" alt="Логотип" />
+        </div>
+        <div class="menu">
+            <a href="#" class="menu__item menu_search">
+                <img src="public/images/search.svg" alt="Поиск" />
+                Поиск книг
+            </a>
+
+            <a href="#favorites" class="menu__item">
+                <img src="public/images/favorites.svg" alt="Избранное" />
+                Избранное
+                <div class="menu__counter">
+                    <div class="menu__digits">
+                        ${this.appState.favorites.length}
+                    </div>
+                </div>
+            </a>
+        </div>
         `;
+        return this.el;
     }
 }
